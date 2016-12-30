691eefb532112aa09f923ed7db4a65e409e0dd6e
Teo Sartori <m@sartori.dk>
2016-12-31 00:27:17 +0100

    Further migration.

--- FilesToIpfs/FilesToIpfs.swift
+++ FilesToIpfs/FilesToIpfs.swift
@@ -19,19 +19,173 @@ class FilesToIpfs : NSViewController {
     }
     
     @IBOutlet var filesToIpfsView: FilesToIpfsView!
+    @IBOutlet weak var filePathTable: NSTableView!
     
     var args: [String]!
     
     public func set(filePaths: [String]) {
         args = filePaths
-        print("wtf \(self)")
         print("args is \(args)")
     }
     
     override func viewDidLoad() {
-        print("tada")
+        
+        /// set up a gesture recognizer for the view's path field
+        let clickRecognizer = NSClickGestureRecognizer(target: self, action: #selector(FilesToIpfs.changeLogFilepath))
+        filesToIpfsView.pathToLogfile.addGestureRecognizer(clickRecognizer)
+
+        filesToIpfsView.logState = UserDefaults.standard.object(forKey: "FilesToIpfsLogPreference") as? Int ?? NSOffState
+        filesToIpfsView.logPath = UserDefaults.standard.object(forKey: "FilesToIpfsLogFilePath") as? String ?? ""
     }
     
+    func changeLogFilepath() {
+        let pathPanel = NSOpenPanel()
+        pathPanel.title = "Log file selector"
+        pathPanel.message = "Select log file"
+        pathPanel.directoryURL = URL(fileURLWithPath: "/Users/teo")
+        
+        pathPanel.canChooseDirectories = false
+        pathPanel.canCreateDirectories = false
+        pathPanel.allowsMultipleSelection = false
+        pathPanel.allowedFileTypes = ["txt"]
+        
+        if pathPanel.runModal() == NSModalResponseOK {
+            /// change the path to the value
+            if let result = pathPanel.url {
+                filesToIpfsView.logPath = result.path
+                /// Store the path label
+                UserDefaults.standard.setValue(filesToIpfsView.logPath, forKey: "FilesToIpfsLogFilePath")
+            }
+        }
+    }
+    
+    ////////////////////////
+    func storeToLog(hashes: [String], filePaths: [String]) {
+        do {
+            
+            guard FileManager.default.fileExists(atPath: filesToIpfsView.logPath) == true else {
+                throw TGError.generic("Error: log file not found")
+            }
+            
+            let entries = try self.formatHashes(hashes: hashes, paths: filePaths)
+            print("formatted: \(entries)")
+            
+            let validURL = URL(fileURLWithPath: filesToIpfsView.logPath)
+            try self.append(entries: entries, to: validURL)
+            
+            exit(EXIT_SUCCESS)
+            
+        } catch {
+            print("Error: \(error)")
+            exit(EXIT_FAILURE)
+        }
+        
+    }
+    
+    func selectedFiles(selectedIndexes: IndexSet, from files: [String]) -> [String] {
+        
+        var filePaths = [String]()
+        for i in selectedIndexes {
+            
+            let filePath = "file://" + args[i]
+            filePaths.append(filePath)
+        }
+        return filePaths
+    }
+    
+    func formatHashes(hashes: [String], paths: [String]) throws -> [String] {
+        
+        guard hashes.count == paths.count else { throw TGError.generic("hashes and paths don't match") }
+        
+        let locDate = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .long)
+        
+        let hashEntries = hashes.enumerated().map { (index, element) -> String in
+            
+            let filename = NSString(string: paths[index]).lastPathComponent
+            
+            return "\nadded \(element) \(filename) \(locDate)"
+        }
+        
+        return hashEntries
+    }
+    
+    func append(entries: [String], to file: URL) throws {
+        
+        let fileHandle = try FileHandle(forWritingTo: file)
+        defer { fileHandle.closeFile() }
+        
+        fileHandle.seekToEndOfFile()
+        for entry in entries {
+            if let dat = entry.data(using: .utf8) {
+                fileHandle.write(dat)
+            }
+        }
+    }
+    
+    
+//    func toggleLogToFile() {
+//        let state = logToFile.state == NSOnState ? "On" : "Off"
+//        
+//        if logToFile.state == NSOffState { pathLabel.isHidden = true } else { pathLabel.isHidden = false }
+//        UserDefaults.standard.setValue(logToFile.state, forKey: "FilesToIPFSLogPreference")
+//    }
+    
+    func textView(_ textView: NSTextView, clickedOn cell: NSTextAttachmentCellProtocol, in cellFrame: NSRect, at charIndex: Int) {
+        print("click")
+    }
+    
+    func copyToClipboard(hashes: [String]) {
+        /// Write the selected hashes to the system pasteboard.
+        if hashes.count > 0 {
+            let pb = NSPasteboard.general()
+            pb.clearContents()
+            pb.writeObjects(hashes as [NSPasteboardWriting])
+        }
+    }
+    
+    /// Asynchronously generate hashes from the given filepaths.
+    /// Calls the handler on success.
+//    func generateHashes(from filePaths: [String], completionHandler: @escaping ([String]) -> Void ) {
+//        // FIXME: Ensure there actually are files at the filePaths.
+//        var hashes = [String]()
+//        
+//        do {
+//            let api = try IpfsApi(host: "127.0.0.1", port: 5001)
+//            
+//            try api.add(filePaths) { result in
+//                
+//                for index in 0 ..< filePaths.count {
+//                    hashes.append(b58String(result[index].hash!))
+//                }
+//                
+//                completionHandler(hashes)
+//                
+//            }
+//        } catch {
+//            print("error generating hashes \(error)")
+//            exit(EXIT_FAILURE)
+//        }
+//    }
+    ///////////////////////
+    
+    @IBAction func addAction(_ sender: NSButton) {
+        Swift.print("add")
+        
+        guard let selectees = (args as NSArray).objects(at: filePathTable.selectedRowIndexes) as? [String], selectees.count > 0 else { return }
+        
+        let filePaths = selectees.map { "file://" + $0 }
+        print("filepaths \(filePaths)")
+//        generateHashes(from: filePaths) { hashes in
+//            
+//            self.copyToClipboard(hashes: hashes)
+//            
+//            if self.filesToIpfsView.logState == NSOffState { exit(EXIT_SUCCESS) }
+//            
+//            self.storeToLog(hashes: hashes, filePaths: filePaths)
+//        }
+        
+    }
+
     override func awakeFromNib() {
         print("awake")
     }
@@ -62,11 +216,10 @@ extension FilesToIpfs : NSTableViewDelegate {
 
 @available(OSX 10.12, *)
 extension FilesToIpfs : NSTableViewDataSource {
+    
     func numberOfRows(in tableView: NSTableView) -> Int {
-        print("wtf \(self)")
         return args.count
     }
-    
     
     func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
         return args[row]
@@ -75,20 +228,37 @@ extension FilesToIpfs : NSTableViewDataSource {
 
 
 class FilesToIpfsView : NSView {
-    @IBOutlet weak var pathToLogfile: NSTextField!
     
+    @IBOutlet weak var pathToLogfile: NSTextField!
+    @IBOutlet weak var logToFile: NSButton!
+
+    var logState: Int {
+        set {
+            logToFile.state = newValue
+            pathToLogfile.isHidden = (newValue == NSOffState)
+        }
+        get { return logToFile.state }
+    }
+    
+    var logPath: String {
+        set {
+            pathToLogfile.stringValue = newValue
+        }
+        get { return pathToLogfile.stringValue }
+    }
     
     @IBAction func logToFile(_ sender: NSButton) {
-        Swift.print("log toggle")
+        
+        pathToLogfile.isHidden = (logToFile.state == NSOffState)
+        
+        UserDefaults.standard.setValue(logToFile.state, forKey: "FilesToIpfsLogPreference")
+
     }
     
     @IBAction func cancelAction(_ sender: NSButton) {
         Swift.print("cancel")
     }
     
-    @IBAction func addAction(_ sender: NSButton) {
-        Swift.print("add")
-    }
     
     
     @IBAction func changePathToLogfile(_ sender: NSTextField) {
